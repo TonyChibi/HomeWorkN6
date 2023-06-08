76489915f114fb17022afd99f7335f7978668f94
GlazHorsez <to5gogol@gmail.com>
2023-06-09 01:48:03 +0300

    add task n43

---
+++ task43/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+double [] intersection(double []arr1,double []arr2){
+    double pre=arr1[0]-arr2[0];
+    if (pre<0)pre*=-1;
+    double x=pre/(arr1[1]-arr2[1]);
+    double y=arr1[1]*x+arr1[0];
+    double []arr={x,y};
+    return arr;
+}
+
+Console.Write("insert k1: ");
+double k1=double.Parse(Console.ReadLine()!);
+Console.Write("insert b1: ");
+double b1=double.Parse(Console.ReadLine()!);
+
+Console.Write("insert k2: ");
+double k2=double.Parse(Console.ReadLine()!);
+Console.Write("insert b2: ");
+double b2=double.Parse(Console.ReadLine()!);
+double[]arr1={b1,k1};
+double[]arr2={b2,k2};
+Console.WriteLine($"the intersection is [{string.Join("; ",intersection(arr1,arr2))}] ");
