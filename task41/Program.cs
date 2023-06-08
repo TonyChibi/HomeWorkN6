91b47ac57ba4f87cef59b7c9ffeb78a8940fbf02
GlazHorsez <to5gogol@gmail.com>
2023-06-09 01:15:04 +0300

    add task n 41

---
+++ task41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+bool state=true;
+void Counter(){
+    Console.WriteLine("insert some integer positive or negative or 0 to stop: ");
+    int n=0;
+    int count=0;
+    while(state){
+    n=int.Parse(Console.ReadLine()!);
+    if(n>0)count++;
+    else if(n==0)state=false;
+    }
+
+    Console.WriteLine($"{count} positive integers");
+}
+Counter();
